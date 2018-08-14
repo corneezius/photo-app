fff5ef3b2f1d84f0d61045056624035303652ef2
Corneezius <cfr2116@columbia.edu>
2018-08-14 19:06:40 -0400

    starter files

---
+++ src/components/main.js
@@ -0,0 +1,13 @@
+import React, { Component} from "react";
+import ReactDOM from "react-dom";
+import Title from "./Title";
+
+class Main extends Component {
+  render() {
+    return <div>
+              <Title title= {"photowall"} />
+            </div>
+  }
+}
+
+export default Main;

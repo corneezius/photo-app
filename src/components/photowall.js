91dc624b200e6e9868bd53b1c18b0573a68b8868
Corneezius <cfr2116@columbia.edu>
2018-08-19 19:59:45 -0400

    fix sorting

--- src/components/photowall.js
+++ src/components/photowall.js
@@ -10,7 +10,7 @@ function Photowall(props) {
           <div className="photo-grid">
           {props.posts
             .sort(function(x,y) {
-              return y.id -x.id
+              return y.id - x.id  
             })
             .map((post, index) => <Photo key= {index} post={post} onRemovePhoto= {props.onRemovePhoto}
           onNavigate = {props.navigate}/>)}

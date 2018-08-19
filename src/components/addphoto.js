7ff00a8039a7361dfdf03bab7c56b7d372f66669
Corneezius <cfr2116@columbia.edu>
2018-08-19 18:14:13 -0400

    add photo ui and router

--- src/components/addphoto.js
+++ src/components/addphoto.js
@@ -3,8 +3,18 @@ import ReactDOM from "react";
 
 class AddPhoto extends Component {
   render () {
-    return <h1> This is the page where we will add photos </h1>
-  }
+    return (
+    <div>
+      <h1> Photowall </h1>
+      <div className="form">
+        <form>
+          <input type="text" placeholder="link" />
+          <input type="text" placeholder="description" />
+          <button>Post</button>
+        </form>
+      </div>
+    </div>
+  )}
 }
 
 export default AddPhoto;

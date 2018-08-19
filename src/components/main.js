de4f99e60e58556ac0033189dbfc3fe491e225f3
Corneezius <cfr2116@columbia.edu>
2018-08-19 19:47:20 -0400

    update addbutton with route and state change

--- src/components/main.js
+++ src/components/main.js
@@ -37,6 +37,12 @@ class Main extends Component {
     }))
   }
 
+  addPhoto(postSubmitted) {
+    this.setState((state) => ({
+      posts: state.posts.concat([postSubmitted])
+    }))
+  }
+
 
   render() {
     return  (<div>
@@ -47,7 +53,12 @@ class Main extends Component {
                 </div>
               )}/>
 
-            <Route path = "/AddPhoto" component = {AddPhoto}/>
+            <Route path = "/AddPhoto" render = {({history}) => (
+              <AddPhoto onAddPhoto= {(addedPost) => {
+                  this.addPhoto(addedPost)
+                  history.push('/')
+              }}/>
+            )} />
         </div>
   )}
 }

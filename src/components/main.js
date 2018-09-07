4d0023d2068648a031c952f49501868019fba039
Corneezius <cfr2116@columbia.edu>
2018-09-07 12:38:26 -0400

    update addpost action creator

--- src/components/main.js
+++ src/components/main.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import Title from "./title";
 import Photowall from "./photowall";
 import AddPhoto from "./addphoto";
-import {Route} from "react-router-dom"
+import {Route, Link} from "react-router-dom"
 import {removePost} from "../redux/actions"
 
 class Main extends Component {
@@ -17,14 +17,20 @@ class Main extends Component {
     console.log(this.props)
     return  (
           <div>
+            <h1>
+              <Link to="/"> Photowall</Link>
+            </h1>
               <Route exact path = "/" render={() => (
                 <div>
-                  <h1><Title title= {"PushforGorilla"} /></h1>
                   <Photowall {...this.props}/>
                 </div>
 
               )}/>
-              </div>
+
+             <Route path ="/AddPhoto" render = {({history}) => (
+               <AddPhoto {...this.props} onHistory={history}/>
+                  )}/>
+          </div>
             )}
         }
 
